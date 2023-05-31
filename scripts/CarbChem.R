1983ef11ec0c4872005eb51cb873981dd39dcda7
Jill Ashey <jillashey@Jills-MacBook-Pro.local>
2023-05-30 23:42:34 -0400

    carb chem panel plot

--- scripts/CarbChem.R
+++ scripts/CarbChem.R
@@ -10,6 +10,7 @@ library(multcomp)
 library(car)
 library(readxl)
 library(seacarb)
+library(cowplot)
 
 # Set working directory 
 setwd("/Users/jillashey/Desktop/PutnamLab/Repositories/Hawaii2022_pH_Temp_Mcap_Pacu/")
@@ -42,48 +43,211 @@ test <- orion %>%
 
 # Calculate CO2 parameters using seacarb 
 carb.output <- carb(flag=8, var1=all$pH_name, var2=all$TA_Average, S= all$salinity_name, T=all$temp_name, P=0, Pt=0, Sit=0, pHscale="T", kf="pf", k1k2="l", ks="d") #calculate seawater chemistry parameters using seacarb
-carb.output$ALK <- carb.output$ALK*1000000 #convert to µmol kg-1
-carb.output$CO2 <- carb.output$CO2*1000000 #convert to µmol kg-1
-carb.output$HCO3 <- carb.output$HCO3*1000000 #convert to µmol kg-1
-carb.output$CO3 <- carb.output$CO3*1000000 #convert to µmol kg-1
-carb.output$DIC <- carb.output$DIC*1000000 #convert to µmol kg-1
+# carb.output$ALK <- carb.output$ALK*1000000 #convert to µmol kg-1
+# carb.output$CO2 <- carb.output$CO2*1000000 #convert to µmol kg-1
+# carb.output$HCO3 <- carb.output$HCO3*1000000 #convert to µmol kg-1
+# carb.output$CO3 <- carb.output$CO3*1000000 #convert to µmol kg-1
+# carb.output$DIC <- carb.output$DIC*1000000 #convert to µmol kg-1
 carb.output <- carb.output[,-c(1,4,5,8,10:13,19)] #subset variables of interest
 carb.output <- cbind(all$treatment, all$squaricle, all$ID, carb.output) # combine sample info with seacarb output 
 colnames(carb.output) <- c("Treatment", "Squarical", "ID", "Salinity", "Temperature", "pH", "CO2", "pCO2", "HCO3", "CO3", "DIC", "TA", "AragSat")
 
+# Sort treatment levels
+carb.output$Treatment <- factor(carb.output$Treatment, levels = c("control", "medium", "high"))
 
-## Plot by each parameter 
-ggplot(carb.output, aes(x = Treatment, y = Salinity)) +
-  geom_boxplot()
+# Run ANOVA for each parameter
+## Test for differences in salinity by treatment with one-way ANOVA
+res.aov <- aov(Salinity ~ Treatment, data = carb.output)
+summary(res.aov)
+TukeyHSD(res.aov) # See if there is any differences between specific treatments 
+plot(res.aov, 1) # Check ANOVA assumptions of normality 
+leveneTest(Salinity ~ Treatment, data = carb.output) # Check ANOVA assumptions of homogenity of variance 
+
+## Test for differences in temperature by treatment with one-way ANOVA
+res.aov <- aov(Temperature ~ Treatment, data = carb.output)
+summary(res.aov)
+TukeyHSD(res.aov) # See if there is any differences between specific treatments 
+plot(res.aov, 1) # Check ANOVA assumptions of normality 
+leveneTest(Treatment ~ Treatment, data = carb.output) # Check ANOVA assumptions of homogenity of variance 
+
+## Test for differences in pH by treatment with one-way ANOVA
+res.aov <- aov(pH ~ Treatment, data = carb.output)
+summary(res.aov)
+TukeyHSD(res.aov) # See if there is any differences between specific treatments 
+plot(res.aov, 1) # Check ANOVA assumptions of normality 
+leveneTest(pH ~ Treatment, data = carb.output) # Check ANOVA assumptions of homogenity of variance 
+
+## Test for differences in CO2 by treatment with one-way ANOVA
+res.aov <- aov(CO2 ~ Treatment, data = carb.output)
+summary(res.aov)
+TukeyHSD(res.aov) # See if there is any differences between specific treatments 
+plot(res.aov, 1) # Check ANOVA assumptions of normality 
+leveneTest(CO2 ~ Treatment, data = carb.output) # Check ANOVA assumptions of homogenity of variance 
+#### VARIANCE IS NOT HOMOGENOUS
+
+## Test for differences in pCO2 by treatment with one-way ANOVA
+res.aov <- aov(pCO2 ~ Treatment, data = carb.output)
+summary(res.aov)
+TukeyHSD(res.aov) # See if there is any differences between specific treatments 
+plot(res.aov, 1) # Check ANOVA assumptions of normality 
+leveneTest(pCO2 ~ Treatment, data = carb.output) # Check ANOVA assumptions of homogenity of variance 
+
+## Test for differences in HCO3 by treatment with one-way ANOVA
+res.aov <- aov(HCO3 ~ Treatment, data = carb.output)
+summary(res.aov)
+TukeyHSD(res.aov) # See if there is any differences between specific treatments 
+plot(res.aov, 1) # Check ANOVA assumptions of normality 
+leveneTest(HCO3 ~ Treatment, data = carb.output) # Check ANOVA assumptions of homogenity of variance 
+
+## Test for differences in CO3 by treatment with one-way ANOVA
+res.aov <- aov(CO3 ~ Treatment, data = carb.output)
+summary(res.aov)
+TukeyHSD(res.aov) # See if there is any differences between specific treatments 
+plot(res.aov, 1) # Check ANOVA assumptions of normality 
+leveneTest(CO3 ~ Treatment, data = carb.output) # Check ANOVA assumptions of homogenity of variance 
+
+## Test for differences in DIC by treatment with one-way ANOVA
+res.aov <- aov(DIC ~ Treatment, data = carb.output)
+summary(res.aov)
+TukeyHSD(res.aov) # See if there is any differences between specific treatments 
+plot(res.aov, 1) # Check ANOVA assumptions of normality 
+leveneTest(DIC ~ Treatment, data = carb.output) # Check ANOVA assumptions of homogenity of variance 
+
+## Test for differences in TA by treatment with one-way ANOVA
+res.aov <- aov(TA ~ Treatment, data = carb.output)
+summary(res.aov)
+TukeyHSD(res.aov) # See if there is any differences between specific treatments 
+plot(res.aov, 1) # Check ANOVA assumptions of normality 
+leveneTest(TA ~ Treatment, data = carb.output) # Check ANOVA assumptions of homogenity of variance 
+
+## Test for differences in AragSat by treatment with one-way ANOVA
+res.aov <- aov(AragSat ~ Treatment, data = carb.output)
+summary(res.aov)
+TukeyHSD(res.aov) # See if there is any differences between specific treatments 
+plot(res.aov, 1) # Check ANOVA assumptions of normality 
+leveneTest(AragSat ~ Treatment, data = carb.output) # Check ANOVA assumptions of homogenity of variance 
+
+
+
+
+
+
+
+
+
+# Plot by each parameter 
+salinity_plot <- ggplot(carb.output, aes(x = Treatment, y = Salinity, fill = Treatment)) +
+  geom_boxplot() + 
+  scale_fill_manual(values = c("lightblue", "lightgreen", "pink")) +
+  ylab("Salinity (psu)") +
+  xlab("") +
+  theme_classic() + 
+  theme(axis.text = element_text(size=12, color="black"), 
+        axis.title=element_text(size=12, color="black", face="bold")) +
+  theme(legend.position = "none")
  
-ggplot(carb.output, aes(x = Treatment, y = Temperature)) +
-  geom_boxplot() 
+temp_plot <- ggplot(carb.output, aes(x = Treatment, y = Temperature, fill = Treatment)) +
+  geom_boxplot() + 
+  scale_fill_manual(values = c("lightblue", "lightgreen", "pink")) +
+  ylab("Temperature (°C)") +
+  xlab("") +
+  theme_classic() + 
+  theme(axis.text = element_text(size=12, color="black"), 
+        axis.title=element_text(size=12, color="black", face="bold")) +
+  theme(legend.position = "none")
 
-ggplot(carb.output, aes(x = Treatment, y = pH)) +
-  geom_boxplot()  
+pH_plot <- ggplot(carb.output, aes(x = Treatment, y = pH, fill = Treatment)) +
+  geom_boxplot() + 
+  scale_fill_manual(values = c("lightblue", "lightgreen", "pink")) +
+  ylab("pH") +
+  xlab("") +
+  theme_classic() + 
+  theme(axis.text = element_text(size=12, color="black"), 
+        axis.title=element_text(size=12, color="black", face="bold")) +
+  theme(legend.position = "none") 
 
-ggplot(carb.output, aes(x = Treatment, y = CO2)) +
-  geom_boxplot()  
+CO2_plot <- ggplot(carb.output, aes(x = Treatment, y = CO2, fill = Treatment)) +
+  geom_boxplot() + 
+  scale_fill_manual(values = c("lightblue", "lightgreen", "pink")) +
+  ylab("CO2 (µmol kg-1)") +
+  xlab("") +
+  theme_classic() + 
+  theme(axis.text = element_text(size=12, color="black"), 
+        axis.title=element_text(size=12, color="black", face="bold")) +
+  theme(legend.position = "none") 
 
-ggplot(carb.output, aes(x = Treatment, y = pCO2)) +
-  geom_boxplot()
+carb.output_sub <- carb.output[-c(15,16,21,60,68),] # remove outlier 
+pCO2_plot <- ggplot(carb.output_sub, aes(x = Treatment, y = pCO2, fill = Treatment)) +
+  geom_boxplot() + 
+  scale_fill_manual(values = c("lightblue", "lightgreen", "pink")) +
+  ylab("pCO2 (µmol kg-1)") +
+  xlab("") +
+  theme_classic() + 
+  theme(axis.text = element_text(size=12, color="black"), 
+        axis.title=element_text(size=12, color="black", face="bold")) +
+  theme(legend.position = "none")
+## need to remove outlier
 
-ggplot(carb.output, aes(x = Treatment, y = HCO3)) +
-  geom_boxplot()
+HCO3_plot <- ggplot(carb.output, aes(x = Treatment, y = HCO3, fill = Treatment)) +
+  geom_boxplot() + 
+  scale_fill_manual(values = c("lightblue", "lightgreen", "pink")) +
+  ylab("HCO3 (µmol kg-1)") +
+  xlab("") +
+  theme_classic() + 
+  theme(axis.text = element_text(size=12, color="black"), 
+        axis.title=element_text(size=12, color="black", face="bold")) +
+  theme(legend.position = "none") 
 
-ggplot(carb.output, aes(x = Treatment, y = CO3)) +
-  geom_boxplot()
+CO3_plot <- ggplot(carb.output, aes(x = Treatment, y = CO3, fill = Treatment)) +
+  geom_boxplot() + 
+  scale_fill_manual(values = c("lightblue", "lightgreen", "pink")) +
+  ylab("CO3 (µmol kg-1)") +
+  xlab("") +
+  theme_classic() + 
+  theme(axis.text = element_text(size=12, color="black"), 
+        axis.title=element_text(size=12, color="black", face="bold")) +
+  theme(legend.position = "none")
 
-ggplot(carb.output, aes(x = Treatment, y = DIC)) +
-  geom_boxplot()
+DIC_plot <- ggplot(carb.output, aes(x = Treatment, y = DIC, fill = Treatment)) +
+  geom_boxplot() + 
+  scale_fill_manual(values = c("lightblue", "lightgreen", "pink")) +
+  ylab("Dissolved Inorganic Carbon (µmol kg-1)") +
+  xlab("") +
+  theme_classic() + 
+  theme(axis.text = element_text(size=12, color="black"), 
+        axis.title=element_text(size=12, color="black", face="bold")) +
+  theme(legend.position = "none")
 
-ggplot(carb.output, aes(x = Treatment, y = TA)) +
-  geom_boxplot()
+TA_plot <- ggplot(carb.output, aes(x = Treatment, y = TA, fill = Treatment)) +
+  geom_boxplot() + 
+  scale_fill_manual(values = c("lightblue", "lightgreen", "pink")) +
+  ylab("Total Alkalinity (µmol kg-1)") +
+  xlab("") +
+  theme_classic() + 
+  theme(axis.text = element_text(size=12, color="black"), 
+        axis.title=element_text(size=12, color="black", face="bold")) +
+  theme(legend.position = "none")
 
-ggplot(carb.output, aes(x = Treatment, y = AragSat)) +
-  geom_boxplot()
+AragSat_plot <- ggplot(carb.output, aes(x = Treatment, y = AragSat, fill = Treatment)) +
+  geom_boxplot() + 
+  scale_fill_manual(values = c("lightblue", "lightgreen", "pink")) +
+  ylab("Aragonite Saturation (µmol kg-1)") +
+  xlab("") +
+  theme_classic() + 
+  theme(axis.text = element_text(size=12, color="black"), 
+        axis.title=element_text(size=12, color="black", face="bold")) +
+  theme(legend.position = "none")
+
+
+all_plots<-plot_grid(salinity_plot, temp_plot, pH_plot, CO2_plot, pCO2_plot, HCO3_plot, CO3_plot, DIC_plot, TA_plot, AragSat_plot, 
+                     nrow=2, ncol=5, label_y=1, labels=c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J"), label_size=18)
+ggsave(all_plots, file="output/CarbChem/CarbChem_panel.png", width=25, height=10)
 
 
 ### Once I have all the data, I will run stats for all environmental variables by treatment. 
-### I will also make tables for the manucsript 
+### I will also make tables for the manuscript 
+
+
+
+## Plot different way 
 
